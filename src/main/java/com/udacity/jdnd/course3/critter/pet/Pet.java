14b85009d5094f47a7b43d603950ad4d6c661418
“Alan <alan.hillgemann@anz.com>
2021-06-17 06:10:48 +1000

    fix: implement review feedback

--- src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
+++ src/main/java/com/udacity/jdnd/course3/critter/pet/Pet.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Pet {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private LocalDate birthDate;
